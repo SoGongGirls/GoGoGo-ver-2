4e04efceed0fdc0872b8611e48fbdbcf2ab5e172
M1nseoPark <kjms0820@gmail.com>
2023-04-29 11:01:16 +0900

    메인 화면 수정

--- app/src/main/java/com/minseo/gogogo_ver2/MainActivity.kt
+++ app/src/main/java/com/minseo/gogogo_ver2/MainActivity.kt
@@ -1,17 +1,10 @@
 package com.minseo.gogogo_ver2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import com.minseo.gogogo_ver2.databinding.ActivityMainBinding
-import com.minseo.gogogo_ver2.view.fragment.Fragment1
-import com.minseo.gogogo_ver2.view.fragment.Fragment2
-import com.minseo.gogogo_ver2.view.fragment.Fragment3
-
-private const val TAG_FRAGMENT_1 = "fragment1"
-private const val TAG_FRAGMENT_2 = "fragment2"
-private const val TAG_FRAGMENT_3 = "fragment3"
+import com.minseo.gogogo_ver2.view.survey.SurveyStart
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -22,60 +15,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /* fragment 설정 */
-        setFragment(TAG_FRAGMENT_1, Fragment1())
-
-        binding.bottom.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.bottom_navigation_1 -> setFragment(TAG_FRAGMENT_1, Fragment1())
-                R.id.bottom_navigation_2 -> setFragment(TAG_FRAGMENT_2, Fragment2())
-                R.id.bottom_navigation_3 -> setFragment(TAG_FRAGMENT_3, Fragment3())
-            }
-            true
-        }
-    }
-
-    private fun setFragment(tag: String, fragment: Fragment) {
-        val manager: FragmentManager = supportFragmentManager
-        val fragTransaction = manager.beginTransaction()
-
-        if (manager.findFragmentByTag(tag) == null){
-            fragTransaction.add(R.id.container, fragment, tag)
-        }
-
-        val fragment1 = manager.findFragmentByTag(TAG_FRAGMENT_1)
-        val fragment2 = manager.findFragmentByTag(TAG_FRAGMENT_2)
-        val fragment3 = manager.findFragmentByTag(TAG_FRAGMENT_3)
-
-        if (fragment1 != null){
-            fragTransaction.hide(fragment1)
-        }
-
-        if (fragment2 != null){
-            fragTransaction.hide(fragment2)
-        }
-
-        if (fragment3 != null) {
-            fragTransaction.hide(fragment3)
-        }
-
-        if (tag == TAG_FRAGMENT_1) {
-            if (fragment1 != null){
-                fragTransaction.show(fragment1)
-            }
-        }
-        else if (tag == TAG_FRAGMENT_2) {
-            if (fragment2 != null) {
-                fragTransaction.show(fragment2)
-            }
-        }
-
-        else if (tag == TAG_FRAGMENT_3){
-            if (fragment3 != null){
-                fragTransaction.show(fragment3)
-            }
-        }
-
-        fragTransaction.commitAllowingStateLoss()
+        binding.btStart.setOnClickListener({
+            val intent = Intent(this, SurveyStart::class.java)
+            startActivity(intent)
+        })
     }
 }
