9097e739f7226cc9c7c3f0288fac20bf177e22c1
Minseo Park <80569061+M1nseoPark@users.noreply.github.com>
2023-07-31 11:02:17 +0900

    Update store_crawling.py

--- store_crawling.py
+++ store_crawling.py
@@ -1,13 +1,9 @@
 from selenium.webdriver.common.by import By
-from urllib3.util.retry import Retry
-from requests.adapters import HTTPAdapter
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import re
 from selenium.webdriver.common.keys import Keys
 import time
-import requests
-import json
+import csv
 
 url = 'https://map.kakao.com/'
 driver = webdriver.Chrome()  # 드라이버 경로
@@ -27,19 +23,20 @@ time.sleep(2)
 driver.find_element(By.XPATH, '//*[@id="info.main.options"]/li[2]/a').send_keys(Keys.ENTER)
 
 
-def storeNamePrint():
+def storeNamePrint(page):
     time.sleep(0.2)
 
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
 
     store_lists = soup.select('.placelist > .PlaceItem')
+    list = []
 
     for store in store_lists:
         temp = []
         name = store.select('.head_item > .tit_name > .link_name')[0].text
         degree = store.select('.rating > .score > .num')[0].text
-        addr = store.select('.info_item > .addr')[0].text.splitlines()[1]
+        addr = store.select('.info_item > .addr')[0].text.splitlines()[1]  # 도로명주소 
         tel = store.select('.info_item > .contact > .phone')[0].text
 
         print(name, degree, addr, tel, '-')
@@ -50,43 +47,37 @@ def storeNamePrint():
 
         list.append(temp)
 
-    # json 파일로 저장
-    with open('store_data_1.json', 'w', encoding='utf-8') as f:
-        json.dump(list, f, indent=4, ensure_ascii=False)
-    
-    return len(store_lists)
+    if page == 1:
+        f = open('store_list_1.csv', 'w', encoding='utf-8-sig', newline='')
+        writercsv = csv.writer(f)
+        header = ['name', 'degree', 'address', 'tel']
+        writercsv.writerow(header)
+
+        for i in list:
+            writercsv.writerow(i)
+    else:
+        f = open('store_list_1.csv', 'a', encoding='utf-8-sig', newline='')
+        writercsv = csv.writer(f)
+
+        for i in list:
+            writercsv.writerow(i)
 
 
 # 페이지 리스트만큼 크롤링하기 
-page = 1   # 현재 크롤링하는 페이지가 전체에서 몇 번째 페이지인지 
-page2 = 0   # 1~5번째 중 몇 번째인지 
+storeNamePrint(1)
+try:
+    # 장소 더보기 버튼 누르기 
+    btn = driver.find_element(By.CSS_SELECTOR, '.more')   
+    driver.execute_script("arguments[0].click();", btn)
 
-for _ in range(0, 5):
-    # 페이지 넘어가며 출력 
-    try:
-        page2 += 1
-        print('**', page, '**')
+    for i in range(2, 6):
+        # 페이지 넘기기
+        xPath = '//*[@id="info.search.page.no' + str(i) + '"]'
+        driver.find_element(By.XPATH, xPath).send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        driver.find_element(By.XPATH, f'//*[@id="info.search.page.no{page2}"]').send_keys(Keys.ENTER)
-        size = storeNamePrint()
-
-        # 한 페이지에 장소 개수가 15개 미만이라면 해당 페이지는 마지막 페이지
-        if len(size) < 15:
-            break
-		
-        # 다음 버튼을 누를 수 없다면 마지막 페이지
-        if not driver.find_element(By.XPATH, '//*[@id="info.search.page.next"]').is_enabled():
-            break
-
-        # 다섯번째 페이지까지 왔다면 다음 버튼을 누르고 page2 = 0으로 초기화
-        if page2 % 5 == 0:
-            driver.find_element(By.XPATH, '//*[@id="info.search.page.next"]').send_keys(Keys.ENTER)
-            page2 = 0
-
-        page += 1
-    
-    except:
-        print('ERROR!')
-        break
+        storeNamePrint(i)
+except:
+    print('ERROR!')
 
 print('**크롤링 완료**')
