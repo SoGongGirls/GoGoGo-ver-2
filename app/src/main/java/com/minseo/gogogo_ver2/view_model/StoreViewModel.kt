558d68bce82406e8a75e7f0918437081cdfa140b
M1nseoPark <kjms0820@gmail.com>
2023-07-31 22:23:06 +0900

    별점 cast 에러 수정

--- app/src/main/java/com/minseo/gogogo_ver2/view_model/StoreViewModel.kt
+++ app/src/main/java/com/minseo/gogogo_ver2/view_model/StoreViewModel.kt
@@ -9,6 +9,7 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.minseo.gogogo_ver2.model.StoreItem
+import kotlin.text.Typography.degree
 
 class StoreViewModel : ViewModel() {
     var _storeData = MutableLiveData<List<StoreItem>>()
@@ -32,12 +33,24 @@ class StoreViewModel : ViewModel() {
                                 val id : String = item.key.toString()
                                 val name : String = item.child("name").value as String
                                 val address : String = item.child("address").value as String
-                                val degree : Double = item.child("degree").value as Double
                                 val logo : String = item.child("logo").value as String
                                 val latitude : Double = item.child("latitude").value as Double
                                 val longitude : Double = item.child("longitude").value as Double
                                 val tel : String = item.child("tel").value as String
                                 val photo : String = item.child("photo").value as String
+                                var degree = 0.0
+
+                                try {
+                                    degree = item.child("degree").value as Double
+                                }
+                                catch (e: java.lang.ClassCastException) {
+                                    val degree_1 = item.child("degree").value
+
+                                    if (degree_1 is String)
+                                        degree = 0.0
+                                    else if (degree_1 is Long)
+                                        degree = degree_1.toDouble()
+                                }
 
                                 val f1m = StoreItem(id, name, address, degree, logo, latitude, longitude, tel, photo)
                                 newStoreData.add(f1m)
